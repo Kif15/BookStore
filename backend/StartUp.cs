ea6894c6d7cb2b2148815fa092790d6650abba94
K Kifleyesus Musgun Sium <kifleyesus.kifleyesus.musgun.sium@hva.nl>
2025-10-26 12:38:41 +0100

    Update: dowgrading to 3.1 dotnet version
    Tried to fix the error of double entry, but could not be solved. So, last push about dotnet.

--- backend/StartUp.cs
+++ backend/StartUp.cs
@@ -1,71 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http;   // for WriteAsync
-using System.Linq;                 // for Any()
+using Microsoft.Extensions.Logging;
 
 namespace BookStore
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen();
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowFrontend",
-                    builder => builder.WithOrigins("http://localhost:3000")
-                                      .AllowAnyMethod()
-                                      .AllowAnyHeader());
-            });
-
-            services.AddDbContext<BookContext>(options =>
-                options.UseInMemoryDatabase("BookStoreDB"));
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
             }
 
-            app.UseRouting();
+            app.UseHttpsRedirection();
 
-            app.UseCors("AllowFrontend");
+            app.UseRouting();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapGet("/health", async context =>
-                {
-                    await context.Response.WriteAsync("Healthy");
-                });
             });
-
-            // Seed data
-            using (var scope = app.ApplicationServices.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<BookContext>();
-                if (!context.Books.Any())
-                {
-                    context.Books.AddRange(
-                        new Book { Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Price = 12.99m },
-                        new Book { Title = "To Kill a Mockingbird", Author = "Harper Lee", Price = 14.99m },
-                        new Book { Title = "1984", Author = "George Orwell", Price = 13.99m },
-                        new Book { Title = "Pride and Prejudice", Author = "Jane Austen", Price = 11.99m }
-                    );
-                    context.SaveChanges();
-                }
-            }
         }
     }
 }
